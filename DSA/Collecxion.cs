8bcd2b11b51f216c5f4f0004aa14d1f8d6355815
arunkumar.aj <arunkumar.aj@PIC4357>
2024-02-14 14:42:38 +0530

    Add project files.

---
+++ DSA/Collecxion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace DSA
+{
+    internal class Collecxion : CollectionBase
+    {
+        public void Add(object item)
+        {
+            InnerList.Add(item);
+        }
+        public void Remove(object item)
+        {
+            InnerList.Remove(item);
+        }
+        public new void Clear()
+        {
+            InnerList.Clear();
+        }
+        public new int Count()
+        {
+            return InnerList.Count;
+        }
+    }
+}
---
+++ DSA/Program.cs
@@ -0,0 +1,22 @@
+﻿using DSA;
+using System.Collections;
+
+var t1 = new Timing();
+t1.startTime();
+var col1 = new Collecxion();
+for (int i = 0; i < 1_000_000; i++)
+{
+    col1.Add(i);
+}
+t1.StopTime();
+Console.WriteLine("Collecxion " + t1.Result());
+
+var t2 = new Timing();
+t2.startTime();
+var col2 = new ArrayList();
+for (int i = 0; i < 1_000_0000; i++)
+{
+    col2.Add(i);
+}
+t2.StopTime();
+Console.WriteLine("ArrayList " + t2.Result());
